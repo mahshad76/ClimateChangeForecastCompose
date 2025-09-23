09f783c79d6dfcc66bf3e7983720978c24978b62
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-09-23 11:56:42 -0400

    the network module is completed by providing the relative singletons

--- core/network/src/main/java/com/mahshad/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/mahshad/network/di/NetworkModule.kt
@@ -1,11 +1,18 @@
 package com.mahshad.network.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.mahshad.network.ApiService
+import com.mahshad.network.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -22,16 +29,52 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideApiKeyInterceptor(): ApiKeyInterceptor = ApiKeyInterceptor(BuildConfig)
+    fun provideApiKeyInterceptor(): ApiKeyInterceptor =
+        ApiKeyInterceptor(BuildConfig.API_KEY)
 
     @Provides
     @Singleton
-    fun provideOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
-        OkHttpClient.Builder().let { builder ->
-            builder.addInterceptor(httpLoggingInterceptor)
-            builder.connectTimeout(30, TimeUnit.SECONDS)
-            builder.readTimeout(30, TimeUnit.SECONDS)
-            builder.writeTimeout(30, TimeUnit.SECONDS)
-            builder.build()
-        }
+    fun provideOkhttpClient(
+        apiKeyInterceptor: ApiKeyInterceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(apiKeyInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
 }
+
+@Provides
+@Singleton
+fun provideJson() = Json {
+    ignoreUnknownKeys = true
+    isLenient = true
+    prettyPrint = true
+    coerceInputValues = true
+    encodeDefaults = false
+}
+
+@Provides
+@Singleton
+fun provideRetrofitConverter(json: Json): Converter.Factory {
+    val contentType = "application/json".toMediaType()
+    return json.asConverterFactory(contentType)
+}
+
+@Provides
+@Singleton
+fun provideRetrofit(okHttpClient: OkHttpClient, converterFactory: Converter.Factory): Retrofit =
+    Retrofit
+        .Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(converterFactory)
+        .build()
+
+@Provides
+@Singleton
+fun provideApiService(retrofit: Retrofit): ApiService =
+    retrofit.create(ApiService::class.java)
