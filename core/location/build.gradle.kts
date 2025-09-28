47f41b31fca28afa60eb1084eb538cc564e552ec
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-09-28 16:38:23 -0400

    the location permissions are added to the manifest of the location module and the google play service location lib is added

--- core/location/build.gradle.kts
+++ core/location/build.gradle.kts
@@ -34,6 +34,9 @@ android {
 
 dependencies {
 
+    // Google Play Services location library
+    implementation(libs.play.services.location)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
